7407304625ecd5e67e0e6782460c6d002b246842
Barru Adi <barru.adi@gmail.com>
2024-05-07 22:10:11 +0700

    feat: add interval in RNG

--- F00_RNG.py
+++ F00_RNG.py
@@ -1,5 +1,5 @@
-def RNG():
-    # Fungsi untuk menghasilkan bilangan acak dalam rentang 1-5
+def RNG(interval):
+    # Fungsi untuk menghasilkan bilangan acak dalam rentang interval
     # seed, a, c, m, random_number = integer
     import datetime as time
     a = 16523
@@ -9,5 +9,5 @@ def RNG():
     # Definisikan seed dengan nilai awal waktu (timestamp)
     seed = int(time.datetime.now().timestamp())
     # Menggunakan algoritma LCG untuk menghasilkan bilangan acak dalam rentang 1-5
-    random_number = (((a * seed + c) % m) % 5) + 1
+    random_number = (((a * seed + c) % m) % interval) + 1
     return random_number
